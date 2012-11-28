023865635cd62cc235c47e3b8c9395c14b61da6d
Dave Newman <dave@snappyco.de>
2012-11-27 17:22:59 -0800

    removed ami

--- lib/prism/back/box_type.rb
+++ lib/prism/back/box_type.rb
@@ -1,16 +1,11 @@
 class BoxType
-  attr_reader :id, :ram_mb, :ecus, :ami
-
-  AMIS = {
-    '64bit' => 'ami-1176c278',
-      'HVM' => 'ami-9f9213f6'
-  }
-
+  attr_reader :id, :ram_mb, :ecus
+  
   def self.definitions
     [
-      BoxType.new('m1.small', 1.7 * 1024, 1, AMIS['64bit']),
-      BoxType.new('c1.xlarge', 7.0 * 1024, 20, AMIS['64bit']),
-      BoxType.new('cc2.8xlarge', 60.5 * 1024, 88, AMIS['64bit']),
+      BoxType.new('m1.small', 1.7 * 1024, 1),
+      BoxType.new('c1.xlarge', 7.0 * 1024, 20),
+      BoxType.new('cc2.8xlarge', 60.5 * 1024, 88),
     ]
   end
 
@@ -18,7 +13,7 @@ class BoxType
     definitions.find{|box_type| box_type.id == id }
   end
 
-  def initialize id, ram_mb, ecus, ami
-    @id, @ram_mb, @ecus, @ami = id, ram_mb, ecus, ami
+  def initialize id, ram_mb, ecus
+    @id, @ram_mb, @ecus = id, ram_mb, ecus
   end
 end
