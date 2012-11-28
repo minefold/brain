bab0a251e8de24bb42e2d96f7033f12652f862b5
Dave Newman <dave@snappyco.de>
2012-11-27 20:04:20 -0800

    logging

--- lib/jobs/create_server_job.rb
+++ lib/jobs/create_server_job.rb
@@ -3,7 +3,9 @@ class CreateServerJob
 
   def self.perform reply_key
     id = BSON::ObjectId.new
+    puts "[CreateServerJob] assiged id:#{id}"
     Models::Server.insert(_id: id) do
+      puts "[CreateServerJob] created server id:#{id} reply:#{reply_key}"
       Resque.push 'high', class: 'ServerCreatedJob', args: [reply_key, id.to_s]
     end
   end
