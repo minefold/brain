0b968309f181018c180c9e21da1b2f70201f0148
Dave Newman <dave@snappyco.de>
2012-12-05 03:12:45 -0800

    fix tick job

--- lib/prism/back/events/server_event.rb
+++ lib/prism/back/events/server_event.rb
@@ -164,7 +164,8 @@ module Prism
     # TODO this logic belongs in Minefold, not Party Cloud
     def connected_player_usernames(server_id, *a, &b)
       cb = EM::Callback(*a, &b)
-      redis.smembers("server:#{server_id}:players") do |usernames|
+      op = redis.smembers("server:#{server_id}:players") 
+      op.call do |usernames|
         cb.call usernames
       end
       cb
